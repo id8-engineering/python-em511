92bdfc5299d71eff673467399d1cf28d4dc93c35
Bobo Bäck Engström <bobo@id8-engineering.io>
2025-10-28 13:17:41 +0100

    feat(Hz) Add getter to read Hertz.
    Signed-off-by: Bobo Bäck Engström <bobo@id8-engineering.io>

--- src/em511/test_em511.py
+++ src/em511/test_em511.py
@@ -2,6 +2,7 @@
 
 """Test file for driver."""
 
+from decimal import Decimal
 from unittest.mock import MagicMock
 
 import pytest
@@ -20,13 +21,13 @@ def test_V() -> None:
     mock_result.registers = [0x08FC, 0x0000]
     client.read_input_registers.return_value = mock_result
     value = meter.V
-    assert value == 230
+    assert value == Decimal("230.0")
 
     """Test 2: Should pass."""
     mock_result.registers = [0x9A28, 0x0001]
     client.read_input_registers.return_value = mock_result
     value = meter.V
-    assert value == 10500
+    assert value == Decimal("10500.0")
 
     """Test 3: Should raise exception due to more registers in use than allowed."""
     mock_result.registers = [0x1860, 0x0023, 0x4244]
@@ -52,13 +53,13 @@ def test_get_A() -> None:
     mock_result.registers = [0x2904, 0x0000]
     client.read_input_registers.return_value = mock_result
     value = meter.A
-    assert value == 10.5
+    assert value == Decimal("10.5")
 
     """Test 2: Should pass."""
     mock_result.registers = [0x1860, 0x0023]
     client.read_input_registers.return_value = mock_result
     value = meter.A
-    assert value == 2300
+    assert value == Decimal("2300.0")
 
     """Test 3: Should raise exception due to more registers in use than allowed."""
     mock_result.registers = [0x1860, 0x0023, 0x4244]
@@ -72,6 +73,36 @@ def test_get_A() -> None:
     with pytest.raises(ValueError, match="Input overflow EEE for 32-bit register: "):
         _ = meter.A
 
+def test_get_Hz() -> None:
+    """Test Get HZ."""
+    client = MagicMock()
+    mock_result = MagicMock()
+    mock_result.isError.return_value = False
+    meter = Em511(1, client)
+
+    """Test 1: should pass"""
+    mock_result.registers = [0x08FC]
+    client.read_input_registers.return_value = mock_result
+    value = meter.Hz
+    assert value == Decimal("230.0")
+
+    """Test 2: Should pass."""
+    mock_result.registers = [0x9A28]
+    client.read_input_registers.return_value = mock_result
+    value = meter.Hz
+    assert value == Decimal("3946.4")
+
+    """Test 3: Should raise exception due to more registers in use than allowed."""
+    mock_result.registers = [0x1860, 0x0023, 0x5743]
+    client.read_input_registers.return_value = mock_result
+    with pytest.raises(ValueError, match="Unexpected register count:"):
+        _ = meter.Hz
+
+    """Test 6: Should raise exception if input value exceeds maximum value, display shows 'EEE', 16-bit register."""
+    mock_result.registers = [0x7FFF]
+    client.read_input_registers.return_value = mock_result
+    with pytest.raises(ValueError, match="Input overflow EEE for 16-bit register: "):
+        _ = meter.Hz
 
 def test_get_password() -> None:
     """Test Get password."""
