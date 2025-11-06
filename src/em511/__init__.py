0381b4ff009a716844b6df2333189d9545885cc1
Milad Makdesi <milad@id8-engineering.io>
2025-11-06 18:23:12 +0100

    feat: add publish-to-test-pypi.yml
    Signed-off-by: Milad Makdesi <milad@id8-engineering.io>

--- src/em511/__init__.py
+++ src/em511/__init__.py
@@ -4,6 +4,13 @@ Provides the `Em511` class for reading and writing Modbus registers
 using Carlo Gavazzi EM511 energy meters.
 """
 
+from importlib.metadata import PackageNotFoundError, version
+
 from .em511 import Em511
 
-__all__ = ["Em511"]
+__all__ = ["Em511", "__version__"]
+
+try:
+    __version__ = version("python-em511")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
